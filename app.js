53fddd5c4a9571f87ed3d83b22cb8ff978b5bb0c
Alex Bennett <alexandergraybennett@gmail.com>
2016-09-18 01:44:46 -0600

    makes recursive

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 const content = {
-  copy:  ['lorem ispsum dolor 1','lorem ispsum dolor 2','lorem ispsum dolor 3'],
-  title: ['title ispsum dolor 1','title ispsum dolor 2','title ispsum dolor 3'],
+  copy:  ['lorem ispsum dolor 1','lorem ispsum dolor 2','lorem ispsum dolor 3', 'lorem ispsum dolor 4'],
+  title: ['title ispsum dolor 1','title ispsum dolor 2','title ispsum dolor 3', 'title ispsum dolor 4'],
   links: ['http://vjs.zencdn.net/v/oceans.mp4','http://vjs.zencdn.net/v/oceans.mp4','http://vjs.zencdn.net/v/oceans.mp4','http://vjs.zencdn.net/v/oceans.mp4']
 }
 
 class Carousel {
 
-  constructor(content){
-    this.videoSource = content.links
+  constructor(content) {
     this.videoContainer =  $('.vid-container')
+    this.videoSource = content.links
+    this.videoSourceLength = content.links.length
+    this.titles = content.title
+    this.copy = content.copy
   }
 
   createSlides() {
     var sourceAttributes = this.videoSource;
     let counterLength = this.videoSource.length;
     let suffix = 0;
-    sourceAttributes.forEach( (el, i) => {
-      if(i === 0) this.createVisibleSlide(suffix, el)
-      else this.createInvisibleSlide(suffix, el)
+    sourceAttributes.forEach((el, i) => {
+      if(i === 0) {
+        this.createVisibleSlide(suffix, el)
+      } else {
+        this.createInvisibleSlide(suffix, el)
+      }
       suffix++
     })
   }
@@ -26,73 +32,101 @@ class Carousel {
     var countLength = this.videoSource.length;
     var videoCollection = []
     let counter = 0
+    return new Promise(function(resolve, reject){
       for(var i = 0; i < countLength; i++){
         var currentVid = document.getElementById(`my_video_${ counter }`);
         videoCollection.push(currentVid)
         counter++
       }
-    return videoCollection
+      resolve(videoCollection)
+    })
+  }
+
+  getSlides() {
+    var countLength = this.videoSource.length;
+    var slideCollection = []
+    let counter = 0
+    for(var i = 0; i < countLength; i++) {
+      var currentVid = document.getElementsByClassName(`slide-${ counter }`)[0];
+      slideCollection.push(currentVid)
+      counter++
+    }
+    return slideCollection
   }
 
   playSlides() {
     var vidz = this.getVids()
-    for(var i = 0; i < vidz.length; i++){
-
-      let next = vidz[i + 1]
-      let prev = vidz[i - 1]
-      let curr = vidz[i]
-      let currSlide = document.getElementsByClassName(`slide-${ i }`)[0]
-      let nextSlide = document.getElementsByClassName(`slide-${ i + 1 }`)[0]
-      let prevSlide = document.getElementsByClassName(`slide-${ i - 1 }`)[0]
-
-      console.log(
-        'play next' +
-        ' next ',       next,
-        ' curr ', curr,
-        ' nextSlide ' , nextSlide,
-        ' currSlide ' , currSlide,
-        ' prevSlide ' , prevSlide
-      )
-
-      if(typeof vidz[i + 1] != 'undefined') {
-
-        vidz[i].addEventListener('ended', () => {
-
-          curr.style.display = "none"
-          next.style.display = "flex"
-          next.style.backgroundColor = "blue"
-          currSlide.style.display = "none"
-          nextSlide.style.display = "flex"
-          next.play();
-        })
-
-      } else {
-
-        let vidUno = vidz[0]
-        vidz[i].addEventListener('ended', () => {
-          console.log("play video 1")
-          vidUno.style.display = "flex"
-          vidUno.play()
-        })
+    var slidez = this.getSlides()
+    this.getVids().then(function(vidz) {
+      for(var i = 0; i < vidz.length; i++) {
+        let next = vidz[i + 1]
+        let prev = vidz[i - 1]
+        let curr = vidz[i]
+        let currSlide = document.getElementsByClassName(`slide-${ i }`)[0]
+        let nextSlide = document.getElementsByClassName(`slide-${ i + 1 }`)[0]
+        let prevSlide = document.getElementsByClassName(`slide-${ i - 1 }`)[0]
+        if(typeof vidz[i + 1] != 'undefined') {
+          function playNextSlide() {
+            curr.style.display = "none"
+            next.style.display = "flex"
+            currSlide.style.display = "none"
+            nextSlide.style.display = "flex"
+            next.play();
+          }
+          vidz[i].addEventListener('ended', () => {
+            playNextSlide()
+          })
+        } else {
+          let vidUno = vidz[0]
+          let slideUno = slidez[0]
+          function playFirstSlide() {
+            curr.style.display = "none"
+            currSlide.style.display = "none"
+            slideUno.style.display = 'block'
+            vidUno.style.display = "block"
+            vidUno.play()
+          }
+          vidz[i].addEventListener('ended', () => {
+            playFirstSlide()
+          })
+        }
       }
-    }
+    })
   }
 
   createVisibleSlide(suff, elem) {
     let newSlide = `<div class="slide-${ suff }"></div>`
     this.videoContainer.append(newSlide).css("display", "flex")
-    $(`.slide-${ suff }`).append(`<video id="my_video_0" controls preload="auto" class="slide" src="${ elem }" onended="console.log('its over')"> </video>`)
+    $(`.slide-${ suff }`).append(
+      `<video
+        id="my_video_0"
+        controls preload="auto"
+        class="slide"
+        src="${ elem }"
+        onended="console.log('its over')">
+      </video>`
+    ).append(`<h3>${this.titles[suff]}</h3>`
+    ).append(`<p>${this.copy[suff]}</p>`)
   }
 
   createInvisibleSlide(suff, elem){
     let newSlide = `<div class="slide-${ suff }"></div>`
     this.videoContainer.append(newSlide).css('display', 'flex')
-    $(`.slide-${ suff }`).append(`<video id="my_video_${ suff }" controls preload="auto" class="slide" src="${ elem }"> </video>`).css('display', 'none')
+    $(`.slide-${ suff }`).append(
+      `<video id="my_video_${ suff }"
+        controls preload="auto"
+        class="slide"
+        src="${ elem }">
+      </video>`
+    ).append(`<h3>${this.titles[suff]}</h3>`
+    ).append(`<p>${this.copy[suff]}</p>`
+    ).css(
+      'display', 'none'
+    )
   }
 
   *startCarousel () {
     yield this.createSlides()
-    $('.description').append(content.copy[0])
     var videoOne = document.getElementById("my_video_0");
     videoOne.play()
     yield this.getVids()
@@ -108,10 +142,3 @@ carouselGenerator.next()
 carouselGenerator.next()
 carouselGenerator.next()
 carouselGenerator.next()
-
-$('.dot-one').on(  'mouseover', () => console.log('rolled over 1'))
-$('.dot-two').on(  'mouseover', () => console.log('rolled over 2'))
-$('.dot-three').on('mouseover', () => console.log('rolled over 3'))
-
-function playVid(vid) { vid.play(vid); }
-function pauseVid(vid) { vid.pause(vid); }
